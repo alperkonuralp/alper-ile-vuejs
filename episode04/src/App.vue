1ef86b1afbe7af0fb48fde246103557328bfe4b6
Alper Konuralp <alper@konuralp.gen.tr>
2021-02-28 16:07:57 +0300

    Episode 4 Source Code

---
+++ episode04/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <p>Count = {{ count }}</p>
+    <p>Count = <span v-html="count"></span></p>
+    <button @click="eksilt">Eksilt</button>
+    <button @click="count++">Arttır</button>
+    <hr />
+    <p>message = {{ message }}</p>
+    <p>message = <span v-html="message"></span></p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {
+  private count = 1;
+  private message = "<strong>Merhaba</strong>";
+
+  private eksilt() {
+    this.count--;
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
