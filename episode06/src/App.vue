6771f0fd889d21fbbc54b9779bb663fe48155948
Alper Konuralp <alper@konuralp.gen.tr>
2021-03-14 17:23:30 +0300

    6. Bölüm Kodları

---
+++ episode06/src/App.vue
@@ -0,0 +1,41 @@
+﻿<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"
+                :cities="cities"
+                :countries="countries"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {
+
+  private cities: string[] = ['İzmir', 'İstanbul', 'Ankara', 'Bursa'];
+
+  private countries = {
+    'tr': 'Türkiye',
+    'usa': 'United States Of Amerika',
+    'uk': 'United Kingdom'
+  };
+
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
