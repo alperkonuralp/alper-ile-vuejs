b91ef6d847f3f1da3d500b2e9c625e5d90da005e
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-24 17:54:56 +0300

    12. Bölüm Kodları

---
+++ episode12/src/App.vue
@@ -0,0 +1,34 @@
+﻿<template>
+	<div id="app">
+		<Button msg="Tıklayınız." @tiklandi="butonTiklandi" />
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Vue } from "vue-property-decorator";
+	import Button from "./components/Button.vue";
+
+	@Component({
+		components: {
+			Button,
+		},
+	})
+	export default class App extends Vue {
+
+
+		private butonTiklandi() {
+			console.log("App.Vue Buton Tıklandı.");
+		}
+	}
+</script>
+
+<style lang="scss">
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
---
+++ episode12/src/components/Button.vue
@@ -0,0 +1,20 @@
+﻿<template>
+	<span>
+		<input type="button" :value="msg" @click="butonaBasildi" />
+	</span>
+</template>
+
+<script lang="ts">
+	import { Component, Prop, Vue } from "vue-property-decorator";
+
+	@Component
+	export default class Button extends Vue {
+		@Prop({ default: () => 'Basınız' })
+		private msg!: string;
+
+		private butonaBasildi() {
+			console.log("Butona Basıldı!...");
+			this.$emit("tiklandi");
+		}
+	}
+</script>
