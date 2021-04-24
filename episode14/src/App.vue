3851cfe6d0b9685e057e1c4636a5b762a24b43fe
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-24 18:12:41 +0300

    14.bölüm kodları

---
+++ episode14/src/App.vue
@@ -0,0 +1,42 @@
+﻿<template>
+	<div id="app">
+		<p>
+			<TextBox msg="Adınız" v-model="yazi" />
+		</p>
+		<Button msg="Tıklayınız." @tiklandi="butonTiklandi" />
+		<p>
+			{{ yazi }}
+		</p>
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Vue } from "vue-property-decorator";
+	import Button from "./components/Button.vue";
+	import TextBox from "./components/TextBox.vue";
+
+	@Component({
+		components: {
+			Button,
+			TextBox,
+		},
+	})
+	export default class App extends Vue {
+		private yazi = "Seç";
+
+		private butonTiklandi() {
+			console.log("App.Vue Buton Tıklandı.");
+		}
+	}
+</script>
+
+<style lang="scss">
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
---
+++ episode14/src/components/TextBox.vue
@@ -0,0 +1,26 @@
+﻿<template>
+	<label>
+		{{msg}} : 
+		<input type="text" v-model="value" @input="yaziDegisti($event.target.value)" />
+	</label>
+</template>
+
+<script lang="ts">
+	import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+
+	@Component
+	export default class TextBox extends Vue {
+		@Prop({ default: () => 'Veri' })
+		private msg!: string;
+
+
+		@Prop()
+		private value!: string;
+
+
+		@Emit('input')
+		private yaziDegisti(y:string) {
+			// boş
+		}
+	}
+</script>
