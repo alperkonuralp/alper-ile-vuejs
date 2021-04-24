294d795d22cfacf719792ccda16d25ec7b2ad5b2
Alper Konuralp <alper@konuralp.gen.tr>
2021-04-24 16:49:31 +0300

    11. Bölüm Kodları

---
+++ episode11/src/App.vue
@@ -0,0 +1,40 @@
+﻿<template>
+	<div id="app">
+		<p>Adınız : <input type="text" v-model="yazi" /></p>
+		<p>{{ yazi }}</p>
+		<p>{{ yaziUzunlugu }}</p>
+		<hr/>
+		<ListData/>
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Vue, Watch } from "vue-property-decorator";
+	import ListData from './components/ListData.vue';
+
+	@Component({
+		components: {
+			ListData
+		}
+	})
+	export default class App extends Vue {
+		private yazi = "";
+		private yaziUzunlugu = 0;
+
+		@Watch("yazi")
+		private yazininDegisimi(y: string) {
+			this.yaziUzunlugu = y.length;
+		}
+	}
+</script>
+
+<style lang="scss">
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
---
+++ episode11/src/components/ListData.vue
@@ -0,0 +1,60 @@
+﻿<template>
+	<div class="hello">
+		<h1>Veriler</h1>
+		<p><label>Arama : <input type="text" v-model="filter" /></label></p>
+		<table align="center">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Ad</th>
+					<th>Soyad</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="person in filteredData" :key="person.id">
+					<td>{{person.id}}</td>
+					<td align="left">{{person.firstName}}</td>
+					<td align="left">{{person.lastName}}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script lang="ts">
+	import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+
+	@Component
+	export default class ListData extends Vue {
+		private filter = '';
+
+		private data = [
+			{ id: 1, firstName: 'Alper', lastName: 'Konuralp' },
+			{ id: 2, firstName: 'Burcu', lastName: 'Konuralp' },
+			{ id: 3, firstName: 'Yağmur', lastName: 'Konuralp' },
+			{ id: 4, firstName: 'Ali', lastName: 'Konuralp' },
+			{ id: 5, firstName: 'Çiğdem', lastName: 'Konuralp' },
+			{ id: 6, firstName: 'Ela', lastName: 'Konuralp' },
+			{ id: 7, firstName: 'Demir', lastName: 'Konuralp' },
+		];
+
+		private filteredData: any[] = this.data;
+
+		@Watch("filter")
+		private changefilter(f: string) {
+			if (f) {
+				this.filteredData = this.data
+					.filter((i) => i.firstName.indexOf(f) >= 0
+						|| i.lastName.indexOf(f) >= 0)
+					;
+
+			} else {
+				this.filteredData = this.data;
+			}
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
