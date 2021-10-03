828c4d8bea5334ca703d1d2a66360a84c42628ca
Alper Konuralp <alper@konuralp.gen.tr>
2021-10-03 16:23:31 +0300

    Episode 25

---
+++ episode25/src/App.vue
@@ -0,0 +1,49 @@
+﻿<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <!--<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />-->
+        <ul>
+            <li v-for="eleman in kirtasiyeListesi" :key="eleman">
+                {{eleman}}
+            </li>
+        </ul>
+        
+        <List :liste="kirtasiyeListesi" baslik="Kırtasiye Listesi">
+            <template v-slot:default="{eleman, mesaj}">
+                <span :title="mesaj">deneme - {{eleman}}</span>
+            </template>         
+            <template v-slot:header="{baslik, mesaj}">
+                <span :title="mesaj">
+                    Deneme Listesi [{{baslik}}]
+                </span>
+            </template>
+        </List>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from "vue-property-decorator";
+    import HelloWorld from "./components/HelloWorld.vue";
+    import List from "./components/List.vue";
+
+    @Component({
+        components: {
+            HelloWorld,
+            List,
+        },
+    })
+    export default class App extends Vue {
+        private kirtasiyeListesi = ['Kalem', 'Kağıt', 'Silgi'];
+    }
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ episode25/src/components/List.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="header" v-if="baslik != ''">
+            <slot name="header" 
+                  v-bind:baslik="baslik"
+                  v-bind:mesaj="strmesaj"
+                  >{{baslik}}</slot>
+        </div>
+
+        <ul>
+            <li v-for="eleman in liste" :key="eleman">
+                <!--            <slot v-bind="{ eleman: eleman, mesaj: strmesaj }">{{eleman}}</slot>-->
+                <slot v-bind:eleman="eleman" v-bind:mesaj="strmesaj">
+                    {{eleman}}
+                </slot>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from "vue-property-decorator";
+
+    @Component
+    export default class List extends Vue {
+        @Prop({ type: Array, required: true })
+        private liste!: string[];
+
+        @Prop({ required: false, default: () => '' })
+        private baslik!: string;
+
+        private strmesaj = "Deneme";
+    }
+</script>
+
+<style scoped lang="scss">
+    div.header {
+        font-weight: bold;
+    }
+</style>
