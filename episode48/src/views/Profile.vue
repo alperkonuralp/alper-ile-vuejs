64a4b4cab20bff29840cab45d7f08421be72e1fa
Alper Konuralp <alper@konuralp.gen.tr>
2022-01-09 14:15:34 +0300

    Episode 48

---
+++ episode48/src/views/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="profile">
+        <h1>This is an profile page</h1>
+    </div>
+</template>
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Profile extends Vue {
+
+    }
+</script>
+
