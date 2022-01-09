656889e6d0629fdd3d63c31b0ff0228b8de5968e
Alper Konuralp <alper@konuralp.gen.tr>
2022-01-09 15:40:50 +0300

    episode 49

---
+++ episode49/src/views/Detail.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="home">
+        <table align="center">
+            <tbody>
+                <tr>
+                    <th align="right">
+                        #
+                    </th>
+                    <td align="left">
+                        {{person.id}}
+                    </td>
+                </tr>
+                <tr>
+                    <th align="right">
+                        Name
+                    </th>
+                    <td align="left">
+                        {{person.name}}
+                    </td>
+                </tr>
+                <tr>
+                    <th align="right">
+                        Position
+                    </th>
+                    <td align="left">
+                        {{person.position}}
+                    </td>
+                </tr>
+                <tr>
+                    <th align="right">
+                        Office
+                    </th>
+                    <td align="left">
+                        {{person.office}}
+                    </td>
+                </tr>
+                <tr>
+                    <th align="right">
+                        Age
+                    </th>
+                    <td align="left">
+                        {{person.age}}
+                    </td>
+                </tr>
+                <tr>
+                    <th align="right">
+                        Start Date
+                    </th>
+                    <td align="left">
+                        {{person['start-date']}}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import DemoData from '@/data.json';
+
+    @Component
+    export default class Detail extends Vue {
+        private person: any = {};
+
+        private mounted() {
+            const id = parseInt(this.$route.params['id']);
+            const people = DemoData.filter(x => x.id === id);
+            if (people.length >= 1) {
+                this.person = people[0];
+            }
+            // not found iþlemi yap
+        }
+    }
+</script>
---
+++ episode49/src/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="home">
+        <table align="center">
+            <thead>
+                <tr>
+                    <th>
+                        #
+                    </th>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Position
+                    </th>
+                    <th>
+                        Office
+                    </th>
+                    <th>
+                        Age
+                    </th>
+                    <th>
+                        
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="person in pageData" :key="person.id">
+                    <td>
+                        <router-link :to="'/detail/' + person.id">{{person.id}}</router-link>
+                    </td>
+                    <td align="left">
+                        <router-link :to="{ name: 'Detail', params: { id: person.id }}">{{person.name}}</router-link>
+                    </td>
+                    <td align="left">
+                        {{person.position}}
+                    </td>
+                    <td align="left">
+                        {{person.office}}
+                    </td>
+                    <td>
+                        {{person.age}}
+                    </td>
+                    <td>
+                        <button @click="sendToDetailPage(person.id)">Detail</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+    import DemoData from '@/data.json';
+
+    @Component
+    export default class Home extends Vue {
+        private pageData = DemoData;
+
+        private sendToDetailPage(id: number) {
+            //// 1. yöntem : elle url tanýlama
+            //this.$router.push('/detail/' + id);
+
+            // 2. yöntem : isminden route kaydýna ulaþarak
+            this.$router.push({ name: 'Detail', params: { 'id': id.toString() } });
+        }
+    }
+</script>
