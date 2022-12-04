4f444de402acc490d463c2a2176efe2d25a0ca0d
Alper Konuralp <alper@konuralp.gen.tr>
2022-12-04 13:48:58 +0300

    Episode 50

---
+++ episode50/src/router/index.ts
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "others" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/about2",
+    name: "about2",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "others2" */ "../views/About2View.vue"),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
